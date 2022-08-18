29d5f312e0c3d1a439c363232a411048babe626b
PoornaSenadheera100 <poornasenadheera100@gmail.com>
2022-08-18 07:11:09 +0530

    Updated MainActivity.java

--- app/src/main/java/com/dreamsoft/poohxox/MainActivity.java
+++ app/src/main/java/com/dreamsoft/poohxox/MainActivity.java
@@ -2,13 +2,42 @@ package com.dreamsoft.poohxox;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.content.pm.ActivityInfo;
+import android.media.MediaPlayer;
+import android.os.Build;
 import android.os.Bundle;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.VideoView;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getSupportActionBar().hide();
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+        getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION);
         setContentView(R.layout.activity_main);
+
+        VideoView videoView = findViewById(R.id.videoView);
+        videoView.setVideoPath("android.resource://" + getPackageName() + "/" + R.raw.intro);
+        videoView.start();
+        videoView.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+            @Override
+            public void onCompletion(MediaPlayer mp) {
+                navigate();
+            }
+        });
     }
+
+    public void navigate(){
+        Intent intent = new Intent(this, Menu.class);
+        startActivity(intent);
+        finish();
+    }
+
 }
